a4463c71976c14e4714a7d7a52c6b1012726446b
David Miller <davidmiller252@gmail.com>
2015-11-13 12:47:37 -0500

    doubly linked neighbors and dd is to centre of door now

--- Room.cpp
+++ Room.cpp
@@ -33,19 +33,23 @@ bool Room::initRoom(float newLength, float newWidth, float newHeight) {
   width = newWidth;
 
   // If this room is the head of the roomtree, set values accordingly
-  if(parent == NULL) {
+  if(neighbor[0] == NULL) {
     dir1v = VECTOR3D(1.0f, 0.0f, 0.0f);
     dir2v = VECTOR3D(0.0f, 0.0f, -1.0f);
     origin = VECTOR3D(0.0f, 0.0f, 0.0f);
   }
   else {
-    // set room vectors to parent and fix based on parents wall id
-    dir1v = parent->dir1v;
-    dir2v = parent->dir2v;
-    origin = calcNewOrigin((parent_wall+1)%4, parent->origin);
-    fixParentVectors(&dir1v,&dir2v,parent_wall);
+    // set room vectors to parent
+    dir1v = neighbor[0]->dir1v;
+    dir2v = neighbor[0]->dir2v;
 
     // fetch origin from parent and calc adjusted one based on wall of parent
+    origin = calcNewOrigin((parent_wall+1)%4, neighbor[0]->origin);
+    // fix parent vectors AFTER origin has been calculated
+    fixParentVectors(&dir1v,&dir2v,parent_wall);
+    // let parent know where you are
+    neighbor[0]->addNeighbor(this, parent_wall);
+
 
   }
 
@@ -58,6 +62,8 @@ bool Room::initRoom(float newLength, float newWidth, float newHeight) {
   return true;
 }
 
+// adds a door dd from origin, rotated to wallid, of height dh, and width dwidth
+// dd is displacement from corner to centre of door
 bool Room::addDoor(int wallid, float dd, float dh, float dwidth) {
   int i;
   doorwall[wallid] = (DoorWall*) calloc (1,sizeof(DoorWall));
@@ -81,15 +87,15 @@ bool Room::addDoor(int wallid, float dd, float dh, float dwidth) {
   // If this wall is supposed to have a door, set the 3 panel dimensions and origins
   if(dd != -1) {
     // Calculate origin of other two panels
-    dw->origin[1] = dw->origin[0] + dw->dir1v*dd + dw->dir2v*dh;
-    dw->origin[2] = dw->origin[0] + dw->dir1v*(dd+dwidth);
-
-    dw->len[0] = dd;
+    dw->len[0] = dd-dwidth/2;
     dw->len[1] = dwidth;
-    dw->len[2] = dw->length-dd-dwidth;
+    dw->len[2] = dw->length-dd-dwidth/2;
     dw->hgt[0] = height;
     dw->hgt[1] = height-dh;
     dw->hgt[2] = height;
+
+    dw->origin[1] = dw->origin[0] + dw->dir1v*dw->len[0] + dw->dir2v*dh;
+    dw->origin[2] = dw->origin[0] + dw->dir1v*(dw->len[0]+dwidth);
   }
 
 
@@ -116,6 +122,8 @@ VECTOR3D Room::calcNewOrigin(int wallid, VECTOR3D origin) {
   if(wallid == 3) return origin + (dir2v*width);
 }
 
+// converts dir1v into an updated dir1v with appropriate rotation about axis up
+// can only rotate wallid multiple of 90deg
 VECTOR3D Room::newDir1(int wallid, VECTOR3D up) {
   int i;
   VECTOR3D result=dir1v;
@@ -125,6 +133,7 @@ VECTOR3D Room::newDir1(int wallid, VECTOR3D up) {
   return result;
 }
 
+// Turns parent vectors into vectors for self
 void Room::fixParentVectors(VECTOR3D * dir1v, VECTOR3D * dir2v, int pwall) {
   int i;
   VECTOR3D tmp;
@@ -136,4 +145,11 @@ void Room::fixParentVectors(VECTOR3D * dir1v, VECTOR3D * dir2v, int pwall) {
     *dir1v = tmp;
   }
   
+}
+
+// adds a neighbor
+bool Room::addNeighbor(Room* newNeighbor, int wallid) {
+  if(neighbor[wallid] != NULL) return false;
+  neighbor[wallid] = newNeighbor;
+  return true;
 }
--- Room.hpp
+++ Room.hpp
@@ -40,23 +40,24 @@ private:
   VECTOR3D origin;
   VECTOR3D dir1v, dir2v;
 
-  Room* parent;
+  // neighbor[0] is always the parent
+  Room* neighbor[4];
   int parent_wall;
 
-  DoorWall* doorwall[4]={NULL,NULL,NULL,NULL};
+  DoorWall* doorwall[4];
   QuadMesh* floor;
 
   VECTOR3D calcNewOrigin(int wallid, VECTOR3D origin);
   VECTOR3D newDir1(int wallid, VECTOR3D up);
   void fixParentVectors(VECTOR3D * dir1v, VECTOR3D * dir2v, int pwall);
-public:
-  Room(Room* newParent=NULL, int pwall=2) {parent = newParent; parent_wall = pwall;}
-  ~Room() {}
 
+public:
+  Room(Room* newParent=NULL, int pwall=2) {neighbor[0] = newParent; parent_wall = pwall;}
+  ~Room() {}
   bool initRoom(float newLength=3.0, float newWidth=4.0, float newHeight=2.0); 
   void draw();
   bool addDoor(int wallid, float dd=1.0, float dh=1.5, float dwidth=1.0);
-
+  bool addNeighbor(Room* newNeighbor, int wallid);
 };
 
 #endif
